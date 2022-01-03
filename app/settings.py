8329935d315593f223849a3407c1acb44a455126
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-01-03 14:49:12 +0300

    add functions for create and validate user account; merge banch with setup flask

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,4 @@
 import os
-import typing as tp
 
 from pathlib import Path
 
@@ -10,12 +9,12 @@ load_dotenv(dotenv_path=env_path)
 
 # FLASK SETTINGS
 SECRET_KEY: str = os.environ.get('SECRET_KEY')  # type: ignore
-FLASK_APP: tp.Optional[str] = os.environ.get('FLASK_APP')
-FLASK_ENV: tp.Optional[str] = os.environ.get('FLASK_ENV')
+FLASK_APP: str = os.environ.get('FLASK_APP')  # type: ignore
+FLASK_ENV: str = os.environ.get('FLASK_ENV')  # type: ignore
 
 # DB SETTINGS
-SQL_HOST: tp.Optional[str] = os.environ.get('SQL_HOST')
-SQL_USER: tp.Optional[str] = os.environ.get('SQL_USER')
-SQL_PASSWORD: tp.Optional[str] = os.environ.get('SQL_PASSWORD')
-SQL_DB_NAME: tp.Optional[str] = os.environ.get('SQL_DB_NAME')
-SQL_PORT: tp.Optional[str] = os.environ.get('SQL_PORT')
+SQL_HOST: str = os.environ.get('SQL_HOST')  # type: ignore
+SQL_USER: str = os.environ.get('SQL_USER')  # type: ignore
+SQL_PASSWORD: str = os.environ.get('SQL_PASSWORD')  # type: ignore
+SQL_DB_NAME: str = os.environ.get('SQL_DB_NAME')  # type: ignore
+SQL_PORT: str = os.environ.get('SQL_PORT')  # type: ignore
