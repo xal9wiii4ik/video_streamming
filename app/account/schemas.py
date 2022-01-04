efbbb9132c4cd2c4f1c5faef2a925395497f5ae6
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-01-04 18:54:17 +0300

    create schemas for tokens

--- app/account/schemas.py
+++ app/account/schemas.py
@@ -10,3 +10,16 @@ class RegisterUser(BaseModel):
     email: constr(max_length=100)  # type: ignore
     first_name: constr(max_length=100)  # type: ignore
     last_name: constr(max_length=100)  # type: ignore
+
+
+class AccessToken(BaseModel):
+    """ Schema for getting access token """
+
+    username: str
+    password: str
+
+
+class RefreshToken(BaseModel):
+    """ Schema for getting refresh token """
+
+    refresh_token: str
