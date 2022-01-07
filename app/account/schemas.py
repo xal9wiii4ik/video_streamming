7b30c3f043e3c5ce142def0d9c401be1eef469c1
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-01-07 15:04:41 +0300

    add schema for account views

--- app/account/schemas.py
+++ app/account/schemas.py
@@ -12,6 +12,15 @@ class RegisterUser(BaseModel):
     last_name: constr(max_length=100)  # type: ignore
 
 
+class AccountData(BaseModel):
+    """ Schema for account """
+
+    username: str = ''
+    email: str = ''
+    first_name: str = ''
+    last_name: str = ''
+
+
 class AccessToken(BaseModel):
     """ Schema for getting access token """
 
