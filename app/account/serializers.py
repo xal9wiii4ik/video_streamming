c18913fd9982b3b333649cabf5e9750c45a92e5d
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-01-07 15:05:15 +0300

    add serializer

---
+++ app/account/serializers.py
@@ -0,0 +1,30 @@
+import typing as tp
+
+from account.schemas import AccountData
+
+
+def account_serializer(model_objects: tp.Any,
+                       many: bool = False) -> tp.Tuple[
+    tp.Union[tp.List[tp.Dict[str, tp.Union[str, bool]]],
+             tp.Dict[str, tp.Union[str, bool]]], int
+]:
+    """
+    Serializer for account
+    Args:
+      model_objects: current Model
+      many: if many models
+    Returns:
+      dict with user data
+    """
+
+    schema = AccountData
+
+    if not many:
+        if model_objects is not None:
+            return schema(**model_objects.__dict__).__dict__, 200
+        return {}, 404
+    else:
+        model_objects_data = []
+        for model_object in model_objects:
+            model_objects_data.append(schema(**model_object.__dict__).__dict__)
+        return model_objects_data, 200
