9c90e5ded22261b508392bacc955c758c97710c8
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-01-19 15:48:31 +0300

    remove type: ignore

--- app/settings.py
+++ app/settings.py
@@ -2,34 +2,34 @@ import os
 import typing as tp
 
 # FLASK SETTINGS
-SECRET_KEY: str = os.environ.get('SECRET_KEY')  # type: ignore
-FLASK_APP: str = os.environ.get('FLASK_APP')  # type: ignore
-FLASK_ENV: str = os.environ.get('FLASK_ENV')  # type: ignore
+SECRET_KEY: tp.Optional[str] = os.environ.get('SECRET_KEY')
+FLASK_APP: tp.Optional[str] = os.environ.get('FLASK_APP')
+FLASK_ENV: tp.Optional[str] = os.environ.get('FLASK_ENV')
 
 # DB SETTINGS
 DATABASE: tp.Optional[str] = os.environ.get('DATABASE')
-POSTGRES_HOST: str = os.environ.get('POSTGRES_HOST')  # type: ignore
-POSTGRES_USER: str = os.environ.get('POSTGRES_USER')  # type: ignore
-POSTGRES_PASSWORD: str = os.environ.get('POSTGRES_PASSWORD')  # type: ignore
-POSTGRES_DB: str = os.environ.get('POSTGRES_DB')  # type: ignore
-POSTGRES_PORT: str = os.environ.get('POSTGRES_PORT')  # type: ignore
+POSTGRES_HOST: tp.Optional[str] = os.environ.get('POSTGRES_HOST')
+POSTGRES_USER: tp.Optional[str] = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD: tp.Optional[str] = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_DB: tp.Optional[str] = os.environ.get('POSTGRES_DB')
+POSTGRES_PORT: tp.Optional[str] = os.environ.get('POSTGRES_PORT')
 
 # TEST DB SETTINGS
-TEST_POSTGRES_HOST: str = 'db-test'
-TEST_POSTGRES_USER: str = 'test_user'
-TEST_POSTGRES_PASSWORD: str = 'test_password'
-TEST_POSTGRES_DB: str = 'test_db'
-TEST_POSTGRES_PORT: str = '5432'
+TEST_POSTGRES_HOST: tp.Optional[str] = 'db-test'
+TEST_POSTGRES_USER: tp.Optional[str] = 'test_user'
+TEST_POSTGRES_PASSWORD: tp.Optional[str] = 'test_password'
+TEST_POSTGRES_DB: tp.Optional[str] = 'test_db'
+TEST_POSTGRES_PORT: tp.Optional[str] = '5432'
 
 # JWT SETTINGS
-ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES'))  # type: ignore
-REFRESH_TOKEN_EXPIRE_MINUTES: int = int(os.environ.get('REFRESH_TOKEN_EXPIRE_MINUTES'))  # type: ignore
+ACCESS_TOKEN_EXPIRE_MINUTES: tp.Optional[int] = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES'))  # type: ignore
+REFRESH_TOKEN_EXPIRE_MINUTES: tp.Optional[int] = int(os.environ.get('REFRESH_TOKEN_EXPIRE_MINUTES'))  # type: ignore
 DEFAULT_ACCESS_TOKEN_EXPIRE_MINUTES = 180
 DEFAULT_REFRESH_TOKEN_EXPIRE_MINUTES = 360
-TOKEN_TYPE: str = os.environ.get('TOKEN_TYPE')  # type: ignore
-TOKEN_ALGORITHM: str = os.environ.get('TOKEN_ALGORITHM')  # type: ignore
-ACCESS_TOKEN_JWT_SUBJECT: str = os.environ.get('ACCESS_TOKEN_JWT_SUBJECT')  # type: ignore
-REFRESH_TOKEN_JWT_SUBJECT: str = os.environ.get('REFRESH_TOKEN_JWT_SUBJECT')  # type: ignore
+TOKEN_TYPE: tp.Optional[str] = os.environ.get('TOKEN_TYPE')
+TOKEN_ALGORITHM: tp.Optional[str] = os.environ.get('TOKEN_ALGORITHM')
+ACCESS_TOKEN_JWT_SUBJECT: tp.Optional[str] = os.environ.get('ACCESS_TOKEN_JWT_SUBJECT')
+REFRESH_TOKEN_JWT_SUBJECT: tp.Optional[str] = os.environ.get('REFRESH_TOKEN_JWT_SUBJECT')
 
 # CONFIGS
 DEVELOPMENT_CONFIGURATION = {
