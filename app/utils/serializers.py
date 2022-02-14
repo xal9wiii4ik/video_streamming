0947cf47a88e9141aa45d13e70e1b44f04791f74
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-02-14 15:55:42 +0300

    add conftest; update serializer; update main.py

--- app/utils/serializers.py
+++ app/utils/serializers.py
@@ -53,7 +53,7 @@ class BaseSerializer(BaseModel):
         for field in fields:
             self.remove_fields.append(field)
 
-    def validate_data_before_create(self) -> serializer_data_type:
+    def validate_data_before_create(self, is_partial_update: bool = False) -> serializer_data_type:
         """
         Validate data before create
         Return:
@@ -65,7 +65,10 @@ class BaseSerializer(BaseModel):
             is_key_in = bool(
                 key in self.read_only_fields or key in self.remove_fields or key in self.exclude_fields
             )
-            if (data[key] == '' or data[key] is None) and key in self.required_model_fields:
+            is_required_field = bool(
+                key in self.required_model_fields and not is_partial_update
+            )
+            if (data[key] == '' or data[key] is None) and not is_key_in and is_required_field:
                 raise SerializerValidationError({key: f'{key} must be not empty'})
             if data[key] == '' or data[key] is None or is_key_in:
                 del data[key]
